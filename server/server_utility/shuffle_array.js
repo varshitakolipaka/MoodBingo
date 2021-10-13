27ee47fc793f12592428910bd52845b02ae5483f
AnushaNathRoy <scribble1001@gmail.com>
2021-10-13 17:54:38 +0400

    heroku app

--- server/server_utility/shuffle_array.js
+++ server/server_utility/shuffle_array.js
@@ -7,7 +7,7 @@
 
  //function to randomise the card selected at every turns
  const shuffleArray = (array)  => {
-  console.log("inside_s1");
+	 
 	for (var i = array.length - 1; i > 0; i--) {
 		var j = Math.floor(Math.random() * (i + 1));
 		var temp = array[i];
